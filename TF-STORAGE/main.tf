8f306f090e5d2903fcc5874b64cb343f39f3e560
rajeshvarade <rajeshvarade23@gmail.com>
2026-04-19 23:48:37 +0530

    storage file added

--- TF-STORAGE/main.tf
+++ TF-STORAGE/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "stgac-batch20" {
   location = "Central India"
 }
 
-resource "azurerm_storage_account" "stgac-batch20-storage" {
+resource "azurerm_storage_account" "stgacbatch20storage" {
   name                     = "rajesh239testingstorage"
   resource_group_name      = azurerm_resource_group.stgac-batch20.name
   location                 = azurerm_resource_group.stgac-batch20.location
@@ -17,4 +17,25 @@ resource "azurerm_storage_account" "stgac-batch20-storage" {
   tags = {
     environment = "staging"
   }
+}
+
+resource "azurerm_storage_container" "container-stgac-batch20" {
+  name                  = "rajesh23conatainer"
+  storage_account_name    = azurerm_storage_account.stgacbatch20storage.name
+  container_access_type = "blob"
+
+  depends_on = [azurerm_storage_account.stgacbatch20storage]
+
+}
+
+resource "azurerm_storage_blob" "blob-stgac" {
+  name                   = "img1"
+  storage_account_name   = azurerm_storage_account.stgacbatch20storage.name
+  storage_container_name = azurerm_storage_container.container-stgac-batch20.name
+  type                   = "Block"
+  source                 = "img1.jpeg"
+
+  content_type           = "image/jpeg"
+
+  depends_on = [azurerm_storage_container.container-stgac-batch20]  
 }
